865d71f75628b2ffb70b4ccd41f654944ae0c511
Aaron Schrab <aaron@schrab.com>
2010-05-07 10:13:52 -0400

    Better handling for multiple matches
    Try to get completion for upto to return full path of all matches.
    Currently doesn't work.

--- zsh.d/R80func_upto.zsh
+++ zsh.d/R80func_upto.zsh
@@ -9,15 +9,22 @@ function upto () {
 
 function __upto () {
   local cur=`builtin pwd -L`
+  read -cA args
+
+  local pattern="$args[2]"
 
   reply=()
   while [[ -n $cur ]]; do
     cur="${cur%/*}"
     local base="${cur##*/}"
-    reply=($reply $base)
+    if [[ $base == (#l)"$pattern"* ]]; then
+      reply=($reply $cur)
+    fi
   done
+
+  #echo $reply
 }
 
 compctl -x \
-    'p[1]' -K __upto -- \
+    'p[1]' -U -K __upto -- \
   upto
