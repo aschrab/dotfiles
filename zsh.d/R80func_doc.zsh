61a230886eb063d98c7960fd29c4b59901f00655
Aaron Schrab <aaron@schrab.com>
2010-08-06 21:04:44 -0400

    Move doc() into functions directory
    Also updated the completion to new style

--- zsh.d/R80func_doc.zsh
+++
@@ -1,9 +0,0 @@
-# doc function to display docs for a package, with completion
-if [[ -d /doc ]]; then
-  doc() { cd /doc/$1 && ls }
-  _doc() { _files -W /doc -/ }
-else
-  doc() { cd /usr/share/doc/$1 && ls }
-  _doc() { _files -W /usr/share/doc -/ }
-fi
-compdef _doc doc
