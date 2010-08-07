da8e253986b590fb78f351d2a311c433c5903fe8
Aaron Schrab <aaron@schrab.com>
2010-08-06 20:08:35 -0400

    Improve completion for `upto`

--- zsh.d/R80func_upto.zsh
+++ zsh.d/R80func_upto.zsh
@@ -2,29 +2,7 @@ function upto () {
   local cur=`builtin pwd -L`
   local pattern="$1"
 
-  local switch="${cur%/$pattern/*}/$pattern"
+  local switch="${cur%/$pattern/*}/$pattern/$2"
 
   cd "$switch"
 }
-
-function __upto () {
-  local cur=`builtin pwd -L`
-  read -cA args
-
-  local pattern="$args[2]"
-
-  reply=()
-  while [[ -n $cur ]]; do
-    cur="${cur%/*}"
-    local base="${cur##*/}"
-    if [[ $base == (#l)"$pattern"* ]]; then
-      reply=($reply $cur)
-    fi
-  done
-
-  #echo $reply
-}
-
-compctl -x \
-    'p[1]' -U -K __upto -- \
-  upto
