05b0245a31871480362feef99839ca9210cc59d5
Aaron Schrab <aaron@schrab.com>
2011-03-27 13:00:18 -0400

    zsh: Help for git, svn, sudo subcommands

--- zsh.d/R90help.zsh
+++ zsh.d/R90help.zsh
@@ -2,5 +2,8 @@ if [ -d /usr/share/zsh/help ]
 then
 	unalias run-help 2> /dev/null || :
 	autoload run-help
+	autoload run-help-git
+	autoload run-help-svn
+	autoload run-help-sudo
 	HELPDIR=/usr/share/zsh/help
 fi
