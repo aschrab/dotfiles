43623277dbb7ccc1f791031b2141548e7a26d4c4
Aaron Schrab <aaron@schrab.com>
2011-04-12 12:08:25 -0400

    zsh: Move compinit out of main zshrc
    Had been run both there and in R89compinit.zsh.  Move the complicated
    version from the main zshrc into the latter file replacing its former
    contents.

--- zsh.d/R89compinit.zsh
+++ zsh.d/R89compinit.zsh
@@ -1,2 +1,14 @@
-autoload -Uz compinit
-compinit
+autoload -U compinit zrecompile
+
+zsh_cache=${HOME}/.zsh_cache
+mkdir -p $zsh_cache
+
+if [ $UID -eq 0 ]; then
+  compinit
+else
+  compinit -d $zsh_cache/zcomp-$HOST
+
+  for f in ~/.zshrc $zsh_cache/zcomp-$HOST; do
+    zrecompile -p $f && rm -f $f.zwc.old
+  done
+fi
