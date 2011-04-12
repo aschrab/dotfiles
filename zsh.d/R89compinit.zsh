3f2fe8af677f2022959a9be046a6309ab47b5dc2
Aaron Schrab <aaron@schrab.com>
2011-04-12 12:12:01 -0400

    zsh: Do compilation of functions for any user
    Previously didn't do this for root to avoid generating root-owned files
    in ~ats.  But, now having sudo set $HOME so the compiled files will go
    to ~root in that case.

--- zsh.d/R89compinit.zsh
+++ zsh.d/R89compinit.zsh
@@ -3,12 +3,8 @@ autoload -U compinit zrecompile
 zsh_cache=${HOME}/.zsh_cache
 mkdir -p $zsh_cache
 
-if [ $UID -eq 0 ]; then
-  compinit
-else
-  compinit -d $zsh_cache/zcomp-$HOST
+compinit -d $zsh_cache/zcomp-$HOST
 
-  for f in ~/.zshrc $zsh_cache/zcomp-$HOST; do
-    zrecompile -p $f && rm -f $f.zwc.old
-  done
-fi
+for f in ~/.zshrc $zsh_cache/zcomp-$HOST; do
+	zrecompile -p $f && rm -f $f.zwc.old
+done
