2c27fa8619e12cfe77f5fd25da81d516c1ccffc7
Aaron Schrab <aaron@schrab.com>
2011-07-18 19:10:00 -0400

    zsh: Always ensure $TERMINFO is set
    OpenIndiana doesn't automatically check ~/.terminfo

--- zsh.d/R30environment.zsh
+++ zsh.d/R30environment.zsh
@@ -6,7 +6,7 @@ then
 fi
 
 # Set $TERMINFO to dir in original user's home
-[[ -z $TERMINFO && -n $SUDO_USER ]] && export TERMINFO=~${SUDO_USER}/.terminfo
+if [[ -z $TERMINFO ]] && export TERMINFO=~${SUDO_USER}/.terminfo
 
 unset MAIL  # set it later in host specific portion
 unset MAILCHECK
