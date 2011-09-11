47fc1162c0cef18c057ad56f6ece31b19b3b6c62
Aaron Schrab <aaron@schrab.com>
2011-09-11 02:31:49 -0400

    zsh - Allow argument to zset_title
    If zset_title is called with an argument use that as the title rather
    than automatically determining the title.

--- zsh.d/R60prompt.zsh
+++ zsh.d/R60prompt.zsh
@@ -37,15 +37,15 @@ zset_title() {
   fi
 
   if [[ $xterm == y || $screen == y ]]; then
-    print -nP "\E]1;%(#.#.$)$host$DEBCHROOT\C-g"
+    print -nP "\E]1;${1:-%(#.#.$)$host$DEBCHROOT}\C-g"
   fi
 
   if [[ $xterm == y ]]; then
-    print -nP "\E]2;%(#.#.$)$host$DEBCHROOT($tty):%~\C-g"
+    print -nP "\E]2;${1:-%(#.#.$)$host$DEBCHROOT($tty):%~}\C-g"
   fi
 
   if [[ $screen == y ]]; then
-    print -nP "\E]2;\En %(#.#.$)$host$DEBCHROOT($tty)!%~\C-g\Ek$host$DEBCHROOT%(#.#.$)%.\E\\"
+    print -nP "\E]2;\En ${1:-%(#.#.$)$host$DEBCHROOT($tty)!%~}\C-g\Ek${1:-$host$DEBCHROOT%(#.#.$)%.}\E\\"
   fi
 }
 precmd_functions+='zset_title'
