6ca7acfba010f30d928068c0e56c57e255a18997
Aaron Schrab <aaron@schrab.com>
2012-02-07 14:13:56 -0500

    zsh - Remove setting of $NNTPSERVER
    I haven't had access to the server that this was set to for almost a
    decade.

--- zsh.d/R30environment.zsh
+++ zsh.d/R30environment.zsh
@@ -21,7 +21,6 @@ export NOPASTE_SERVICES='Gist'
 export MTR_OPTIONS="--order LSD NABMV"
 export DBIC_TRACE_PROFILE=console
 
-export NNTPSERVER="news.execpc.com"
 export PSQL_EDITOR="vim +'set ft=sql'"
 
 if [[ -d "/usr/local/lib/site_perl" ]]; then
