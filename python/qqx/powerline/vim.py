f7047a4d768072eb9627823278fa5e6d0a0b5fa0
Aaron Schrab <aaron@schrab.com>
2013-03-12 00:55:09 -0400

    powerline - Custom segment for vim file encoding
    Create a custom powerline segment to display vim file encoding only if
    it isn't one that is expected.

--- python/qqx/powerline/vim.py
+++ python/qqx/powerline/vim.py
@@ -1,5 +1,6 @@
 from powerline.theme import requires_segment_info
-from powerline.bindings.vim import vim_get_func
+from powerline.bindings.vim import getbufvar
+from powerline.lib import add_divider_highlight_group
 
 @requires_segment_info
 def current_char(segment_info):
@@ -9,3 +10,22 @@ def current_char(segment_info):
         return "0x%02X" % ord(char)
     except Exception, e:
         return "0x00"
+
+@requires_segment_info
+@add_divider_highlight_group('background:divider')
+def file_encoding(segment_info, expected=['utf-8'], unknown_text='unknown'):
+    '''Return file encoding/character set.
+
+    :return: file encoding/character set or None if unknown or missing file encoding
+
+    Divider highlight group used: ``background:divider``.
+    '''
+    enc = getbufvar(segment_info['bufnr'], '&fileencoding')
+
+    if enc in expected:
+        return None
+    else:
+        if enc:
+            return enc
+        else:
+            return unknown_text
