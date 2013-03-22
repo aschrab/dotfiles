405db2d3de6633d0f5a4bb8fa577907c12f17eb6
Aaron Schrab <aaron@schrab.com>
2013-03-22 02:14:14 -0400

    zsh - Simplify setting of $path
    Rather than checking what potential entries of $path actually exist just
    include all of them.  Having non-existent directories included isn't a
    problem, I have no idea why I did that in the first place.
    
    Do the same for $manpath.

--- zsh.d/E20paths.zsh
+++ zsh.d/E20paths.zsh
@@ -1,42 +1,20 @@
-path=()
-manpath=()
-ppath=(
+typeset -aU path
+path=(
   ~/bin
   /usr/local/bin
-  /sw/bin
-  /sw/sbin
   /usr/bin
   /bin
   /usr/local/sbin
   /usr/sbin
   /sbin
-  /usr/X11R6/bin
   /usr/X11/bin
-  /usr/share/bin
-  /usr/adm/bin
-  /usr/local/pilot/bin
-  /usr/ccs/bin
-  /usr/ccs/lib
-  /usr/games
 )
 
-for d in $ppath
-do
-  if [[ -d $d ]]
-  then
-    path=($path $d)
-  fi
+typeset -aU manpath
+manpath=(
+  /usr/local/man
+  /usr/X11/man
+  /usr/share/man
+)
 
-  m=${d%/bin}
-  if [[ $d == $m ]]
-    m="$m/man"
-    if [[ -d $m ]]
-    then
-      manpath=($manpath $m)
-    fi
-  then
-  fi
-done
-
-unset ppath d m
 export PATH MANPATH
