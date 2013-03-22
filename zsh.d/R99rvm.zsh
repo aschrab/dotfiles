53100775a52ca49dfd10c10e55be048320ae4e3c
Aaron Schrab <aaron@schrab.com>
2013-03-22 01:54:40 -0400

    zsh - Don't add RVM bin directory to $path
    Remove explicit addition of RVM bin directory to $path, since sourcing
    the RVM script already handles that.

--- zsh.d/R99rvm.zsh
+++ zsh.d/R99rvm.zsh
@@ -3,6 +3,3 @@
 # Some of its functionality requires that it be run as a shell function
 
 [[ -r ~/.rvm/scripts/rvm ]] && source ~/.rvm/scripts/rvm
-
-# Add RVM bin directory to path
-[[ -d ~/.rvm/bin ]] && path=( $path ~/.rvm/bin )
