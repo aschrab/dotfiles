e4882a0ec1a7ead2e9e38f1217e81363499a8cd2
Aaron Schrab <aaron@schrab.com>
2013-09-10 08:54:18 -0400

    zsh - Enable p4 in vcs_info

--- zsh.d/R50vcs_info.zsh
+++ zsh.d/R50vcs_info.zsh
@@ -1,7 +1,7 @@
 autoload -Uz vcs_info
 precmd_functions+='vcs_info'
 
-zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' enable git p4
 zstyle ':vcs_info:*' stagedstr '✓'
 zstyle ':vcs_info:*' unstagedstr '△'
 #zstyle ':vcs_info:*' use-quilt true
@@ -10,3 +10,6 @@ zstyle ':vcs_info:git:*' check-for-changes true
 # If the VCS is git, don't mention that it'll be assumed
 zstyle ':vcs_info:git:*' formats "[%b]%c%u" '%R'
 zstyle ':vcs_info:git:*' actionformats "[%b|%a]%c%u"
+
+export P4HUSH=1
+P4CONFIG=P4ENV
