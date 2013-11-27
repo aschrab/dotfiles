06193d31b8567cfdd4d49178f08f7c34e8c4f9be
Aaron Schrab <aaron@schrab.com>
2013-11-26 22:08:35 -0500

    Fix typo in zsh syntax highlighting submodule name

--- zsh.d/R99syntax.zsh
+++ zsh.d/R99syntax.zsh
@@ -1,6 +1,6 @@
 ZSH_HIGHLIGHT_HIGHLIGHTERS=( main brackets )
 
 () {
-  local f=$zshrc_dir/../upstreams/syntax-hightlight/zsh-syntax-highlighting.zsh
+  local f=$zshrc_dir/../upstreams/syntax-highlight/zsh-syntax-highlighting.zsh
   [ -r $f ] && source $f
 }
