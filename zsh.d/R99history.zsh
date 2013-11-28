c72e178ddee436e3d96f79e7f5e39388b01b22df
Aaron Schrab <aaron@schrab.com>
2013-11-28 14:20:11 -0500

    zsh - Use global history for searching
    Turn on shared history, and arrange for it to be used only when
    searching.  This will allow arrow keys to be used as I'm used to, but
    also allow me to search for commands without needing to remember in
    which session they were entered.

--- zsh.d/R99history.zsh
+++ zsh.d/R99history.zsh
@@ -1,7 +1,19 @@
 HISTFILE=~/.Zhistory
 SAVEHIST=1000
 setopt inc_append_history
-setopt no_share_history
+setopt share_history
 
 alias import_history='fc -RI'
 alias history_import='fc -RI'
+
+zle-line-init() { zle set-local-history -n 1}
+zle -N zle-line-init
+
+zle-global-history-search() {
+  zle set-local-history -n 0
+  zle .$WIDGET
+}
+zle -N history-incremental-pattern-search-backward zle-global-history-search
+zle -N history-incremental-pattern-search-forward  zle-global-history-search
+zle -N history-incremental-search-backward         zle-global-history-search
+zle -N history-incremental-search-forward          zle-global-history-search
