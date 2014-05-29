cee86d7e59b9de79a8047126443e4475b3d13763
Aaron Schrab <aaron@schrab.com>
2014-05-29 18:17:14 -0400

    zsh - Don't unset TMPDIR for tmux
    Stop unsetting $TMPDIR in tmux alias.  Using a TMPDIR that is different
    than the default causes zsh completion of session names to fail, since
    that uses `command tmux` to get the list, avoiding use of the alias.
    I'd originally started doing that to allow me to use tmux as a login
    shell, but I've since decided against doing that.

--- zsh.d/R90aliases.zsh
+++ zsh.d/R90aliases.zsh
@@ -16,13 +16,10 @@ alias wget='noglob wget'
 # Without this commands where stderr is redirected to /dev/null hang
 # http://sourceforge.net/mailarchive/message.php?msg_id=28004727
 # But with versions of libevent < 2.0 this exposes an even worse bug
-() {
-  if [[ $OSTYPE == linux* ]] && [[ -n $(whence tmux) ]] && ! ldd =tmux | fgrep -q libevent-1.
-  then
-    epoll="EVENT_NOEPOLL=1"
-  fi
-  alias tmux="TMPDIR= ${epoll} command tmux"
-}
+if [[ $OSTYPE == linux* ]] && [[ -n $(whence tmux) ]] && ! ldd =tmux | fgrep -q libevent-1.
+then
+  alias tmux="EVENT_NOEPOLL=1 command tmux"
+fi
 
 # Only the initial arguments to `find` should use globbing.
 #
