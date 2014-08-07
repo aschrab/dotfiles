c07271e2fea4027f6b8c145aa4f6a6901a58dff5
Aaron Schrab <aaron@schrab.com>
2014-08-06 20:01:43 -0400

    zsh - start of config for vcs_info for mercurial

--- zsh.d/R50vcs_info.zsh
+++ zsh.d/R50vcs_info.zsh
@@ -1,7 +1,7 @@
 autoload -Uz vcs_info
 precmd_functions+='vcs_info'
 
-zstyle ':vcs_info:*' enable git #p4
+zstyle ':vcs_info:*' enable git hg #p4
 zstyle ':vcs_info:*' stagedstr '✓'
 zstyle ':vcs_info:*' unstagedstr '△'
 #zstyle ':vcs_info:*' use-quilt true
@@ -10,6 +10,13 @@ zstyle ':vcs_info:git(|-p4|-svn):*' check-for-changes true
 # If the VCS is git, don't mention that it'll be assumed
 zstyle ':vcs_info:git(|-p4|-svn):*' formats "[%b]%c%u" '%R'
 zstyle ':vcs_info:git(|-p4|-svn):*' actionformats "[%b|%a]%c%u"
+
+zstyle ':vcs_info:hg:*' check-for-changes true
+zstyle ':vcs_info:hg:*' get-revision true
+zstyle ':vcs_info:hg:*' get-bookmarks true
+zstyle ':vcs_info:hg:*' use-simple false
+zstyle ':vcs_info:hg:*' hgrevformat "%r:%6.6h"
+zstyle ':vcs_info:hg:*' formats "-hg-[%b](%m)%c%u"
 
 export P4HUSH=1
 export P4CONFIG=P4ENV
