1afdaf4a512ec50809853a312d0f9c4d38af8023
Aaron Schrab <aaron@schrab.com>
2014-08-21 15:14:21 -0400

    zsh - Remove early_usr_bin hack
    A workaround has been found for the broken /usr/software/bin/vim, so
    there's no longer a need to force /usr/bin/vim to come before that.  The
    detection of when to apply that hack was never very good any way.

--- zsh.d/E20paths.zsh
+++ zsh.d/E20paths.zsh
@@ -1,15 +1,10 @@
 typeset -aU path
 
-# Hack, on this host need to use /usr/bin/vim because one in /usr/software/bin
-# is broken.  But on other hosts, copy in /usr/bin is hopelessly outdated.
-[[ $HOST == scmrtp* ]] && early_usr_bin=/usr/bin
-
 path=(
   ~/bin
   ~/.gem/bin
   ~/.rbenv/shims
   /usr/local/bin
-  $early_usr_bin
   /usr/software/bin
   /usr/software/rats/bin
   /usr/software/utils/bin
@@ -21,8 +16,6 @@ path=(
   /sbin
   /usr/X11/bin
 )
-
-unset early_usr_bin
 
 typeset -aU manpath
 manpath=(
