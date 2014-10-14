ca1fbcc39e5c09355e3c4536aaad992eeabd2058
Aaron Schrab <aaron@schrab.com>
2014-10-14 15:35:19 -0400

    sqlplus - Initial version of login.sql

---
+++ login.sql
@@ -0,0 +1,12 @@
+set termout off
+-- SET SQLPROMPT "_USER'@'_CONNECT_IDENTIFIER >"
+COLUMN global_name new_value gname
+SELECT LOWER(USER) || '@' || global_name||CHR(10)||'SQL> ' AS global_name
+FROM   global_name;
+SET SQLPROMPT '&gname'
+
+alter session set nls_date_format         = 'YYYY-MM-DD HH24:MI:SS';
+alter session set nls_timestamp_format    = 'YYYY-MM-DD HH24:MI:SS';
+alter session set nls_timestamp_tz_format = 'YYYY-MON-DD HH24:MI:SSXFF TZR';
+
+set termout on
