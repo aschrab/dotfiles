9a11d510ad0bdb973f8308a74621fc92ce0993b9
Aaron Schrab <aaron@schrab.com>
2015-01-07 08:07:58 -0500

    sqlplus - Modify timestamp formats
    Include fractional seconds, display timezone offset rather than name.

--- login.sql
+++ login.sql
@@ -6,7 +6,7 @@ FROM   global_name;
 SET SQLPROMPT '&gname'
 
 alter session set nls_date_format         = 'YYYY-MM-DD HH24:MI:SS';
-alter session set nls_timestamp_format    = 'YYYY-MM-DD HH24:MI:SS';
-alter session set nls_timestamp_tz_format = 'YYYY-MON-DD HH24:MI:SSXFF TZR';
+alter session set nls_timestamp_format    = 'YYYY-MM-DD HH24:MI:SS.FF';
+alter session set nls_timestamp_tz_format = 'YYYY-MON-DD HH24:MI:SS.FF TZH:TZM';
 
 set termout on
