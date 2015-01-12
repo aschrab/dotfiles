9b7f95908f61707ce61d7ed8f6e49938a4f5a7b8
Aaron Schrab <aaron@schrab.com>
2015-01-12 09:54:33 -0500

    zsh - Avoid hard-coding $HELPDIR
    Calculate $HELPDIR based on $module_path rather than assuming it will
    always be /usr/share/zsh/help .

--- zsh.d/R90help.zsh
+++ zsh.d/R90help.zsh
@@ -1,9 +1,9 @@
-if [ -d /usr/share/zsh/help ]
+HELPDIR=${${module_path[1]%/*}/\/lib\//\/share\/}/help
+if [[ -d $HELPDIR ]]
 then
 	unalias run-help 2> /dev/null || :
 	autoload run-help
 	autoload run-help-git
 	autoload run-help-svn
 	autoload run-help-sudo
-	HELPDIR=/usr/share/zsh/help
 fi
