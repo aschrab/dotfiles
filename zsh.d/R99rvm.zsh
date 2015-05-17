4403b6799593f5c9622a4f5f7819917a1cfe4064
Aaron Schrab <aaron@schrab.com>
2015-05-16 22:54:39 -0400

    zsh - Configure for compiling Ruby on OS X
    Set RUBY_CONFIGURE_OPTS to use readline from brew instead of the
    horrible editline that Apple ships.

--- zsh.d/R99rvm.zsh
+++ zsh.d/R99rvm.zsh
@@ -5,6 +5,12 @@
 if [[ -x ~/.rbenv/bin/rbenv ]] && [[ -d ~/.rbenv/shims ]]; then
   path+=(~/.rbenv/bin)
   eval "$(rbenv init -)"
+
+  if (( $+commands[brew] )); then
+    export RUBY_CONFIGURE_OPTS="--with-readline-dir=`brew --prefix readline`"
+    #RUBY_CONFIGURE_OPTS="$RUBY_CONFIGURE_OPTS --with-openssl-dir=`brew --prefix openssl`"
+    #RUBY_CONFIGURE_OPTS="$RUBY_CONFIGURE_OPTS --with-libyaml-dir=`brew --prefix libyaml`"
+  fi
 elif [[ -r ~/.rvm/scripts/rvm ]]; then
   source ~/.rvm/scripts/rvm
 fi
