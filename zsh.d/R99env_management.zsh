646be6d28e9ec5ed6e813906c257b32645754f7a
Aaron Schrab <aaron@schrab.com>
2015-06-30 20:40:38 -0400

    zsh - Combine env management files
    Use single file for setting up rbenv/rvm and ndenv.

--- zsh.d/R99env_management.zsh
+++ zsh.d/R99env_management.zsh
@@ -1,7 +1,3 @@
-# If RVM script is present, source it
-#
-# Some of its functionality requires that it be run as a shell function
-
 if [[ -x ~/.rbenv/bin/rbenv ]] && [[ -d ~/.rbenv/shims ]]; then
   path+=(~/.rbenv/bin)
   eval "$(rbenv init -)"
@@ -13,4 +9,9 @@ if [[ -x ~/.rbenv/bin/rbenv ]] && [[ -d ~/.rbenv/shims ]]; then
   fi
 elif [[ -r ~/.rvm/scripts/rvm ]]; then
   source ~/.rvm/scripts/rvm
+fi
+
+if [[ -x ~/.ndenv/bin/ndenv ]] && [[ -d ~/.ndenv/shims ]]; then
+  path+=(~/.ndenv/bin)
+  eval "$(ndenv init -)"
 fi
--- zsh.d/R99ndenv.zsh
+++
@@ -1,4 +0,0 @@
-if [[ -x ~/.ndenv/bin/ndenv ]] && [[ -d ~/.ndenv/shims ]]; then
-  path+=(~/.ndenv/bin)
-  eval "$(ndenv init -)"
-fi
