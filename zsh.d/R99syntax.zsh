998d43dfaa0f25f925e20c8e80a4903bc5d6387c
Aaron Schrab <aaron@schrab.com>
2016-07-14 01:27:58 -0400

    zsh - Use real path when source syn highlighting
    Versions newer than the ancient one that I was using will fail if
    sourced using a path that has a symlink and `..` as I had been using.

--- zsh.d/R99syntax.zsh
+++ zsh.d/R99syntax.zsh
@@ -1,6 +1,6 @@
 ZSH_HIGHLIGHT_HIGHLIGHTERS=( main brackets )
 
 () {
-  local f=$zshrc_dir/../upstreams/syntax-highlight/zsh-syntax-highlighting.zsh
+  local f=${zshrc_dir:A}/../upstreams/syntax-highlight/zsh-syntax-highlighting.zsh
   [ -r $f ] && source $f
 }
