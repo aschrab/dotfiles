33d17abf8f85a1641c570c7ef3f4c44ab2d74f8f
Aaron Schrab <aaron@schrab.com>
2016-10-26 18:58:27 -0400

    zsh - Prefer `C.UTF-8` for $LC_COLLATE
    Try to avoid mixing character sets in locale settings. Check if
    `C.UTF-8` locale is supported and, if found, use that for $LC_COLLATE in
    preference to `C`.

--- zsh.d/R02misc.zsh
+++ zsh.d/R02misc.zsh
@@ -4,9 +4,18 @@ trap -
 # Make sure that core files are allowed
 unlimit core
 
+export LANG
 # The collation order for en_US is awful, ignores . and case
 # so, don't use it
-export LANG LC_COLLATE="C"
+() {
+  local loc
+  for loc in C.UTF-8 C.UTF8 C; do
+    if locale -a | fgrep --ignore-case --line-regexp --quiet $loc; then
+      export LC_COLLATE=$loc
+      return
+    fi
+  done
+}
 
 # Make is-at-least function available for use in later config files
 autoload -U is-at-least
