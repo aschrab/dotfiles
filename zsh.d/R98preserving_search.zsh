a00e7ee94a274a436b47868b21452791ceab4f21
Aaron Schrab <aaron@schrab.com>
2017-04-10 23:01:23 -0400

    zsh - Backward search preserving input
    Allow searching history to append to already entered command line.

---
+++ zsh.d/R98preserving_search.zsh
@@ -0,0 +1,14 @@
+autoload -Uz narrow-to-region
+function _history-incremental-preserving-pattern-search-backward
+{
+  local state
+  MARK=CURSOR  # magick, else multiple ^R don't work
+  narrow-to-region -p "$LBUFFER${BUFFER:+»}" -P "${BUFFER:+«}$RBUFFER" -S state
+  zle end-of-history
+  zle .history-incremental-pattern-search-backward
+  narrow-to-region -R state
+}
+zle -N _history-incremental-preserving-pattern-search-backward
+bindkey -M viins "^R" _history-incremental-preserving-pattern-search-backward
+bindkey -M isearch "^R" history-incremental-pattern-search-backward
+bindkey "^S" history-incremental-pattern-search-forward
