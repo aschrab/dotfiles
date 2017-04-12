fab1d305436d9e8103facec22201eb1df42f80b6
Aaron Schrab <aaron@schrab.com>
2017-04-11 22:39:25 -0400

    zsh - Move VCS info to separate prompt line
    Running into too many situations where git branch name + path info will
    wrap.

--- zsh.d/R60prompt.zsh
+++ zsh.d/R60prompt.zsh
@@ -62,10 +62,13 @@ else
   rum=""
 fi
 
+nl='
+'
+
 RPS1=''
 PS1='
-%{$pColor$rev%} %D{%d%b%H:%M:%S}  %1v%n@$host$DEBCHROOT  %~ ${vcs_info_msg_0_}%E %{$rum$fColor%}
-%{$pColor%}%!%(#.#.$)%{$fColor%} '
+%{$pColor$rev%} %D{%d%b%H:%M:%S}  %1v%n@$host$DEBCHROOT  %~%E %{$rum$fColor%}
+%{$pColor%}${vcs_info_msg_0_}%E${vcs_info_msg_0_:+$nl}%{$pColor%}%!%(#.#.$)%{$fColor%} '
 
 PS2='%{$pColor%}%_>%{$fColor%} '
 
