52b54817ee92ad2b73cd546db9f4fbe2584fd0d3
Aaron Schrab <aaron@schrab.com>
2017-10-04 13:51:10 -0400

    zsh - Enable support for bash completion scripts

--- zsh.d/R89compinit.zsh
+++ zsh.d/R89compinit.zsh
@@ -4,6 +4,7 @@ zsh_cache=${HOME}/.zsh_cache
 mkdir -p $zsh_cache
 
 compinit -u -d $zsh_cache/zcomp-$HOST-$ZSH_VERSION
+autoload -U bashcompinit && bashcompinit
 
 for f in ${ZDOTDIR:-$HOME}/.zshrc $zsh_cache/zcomp-$HOST-$ZSH_VERSION; do
 	zrecompile -p $f && rm -f $f.zwc.old
