4d57a24128ec39ebde972972d84650ac7536b37e
Aaron Schrab <aaron@schrab.com>
2017-12-20 22:58:13 -0500

    python - Fix issues in pythonrc.py found by flake8

--- pythonrc.py
+++ pythonrc.py
@@ -7,16 +7,21 @@ import atexit
 
 from pprint import pprint as p
 
+# Avoid errors about unused imports
+assert rlcompleter
+assert p
+
 readline.parse_and_bind('tab: complete')
-histfile = os.path.join( os.environ['HOME'], '.pythonhistory' )
+histfile = os.path.join(os.environ['HOME'], '.pythonhistory')
 try:
     readline.read_history_file(histfile)
 except IOError:
     pass
 
-atexit.register( readline.write_history_file, histfile )
+atexit.register(readline.write_history_file, histfile)
 
 h = [None]
+
 
 class Prompt:
     def __init__(self, prompt='h[%d] >> '):
@@ -24,13 +29,15 @@ class Prompt:
 
     def __str__(self):
         try:
-            if _ not in h: h.append(_)
+            if _ not in h:
+                h.append(_)
         except NameError:
             pass
         return self.prompt % len(h)
 
     def __radd__(self, other):
         return str(other) + str(self)
+
 
 sys.ps1 = Prompt()
 sys.ps2 = '...     '
