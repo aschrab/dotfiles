c1a5209091e8416d1ab5d62970dc96f65488e4be
Aaron Schrab <aaron@schrab.com>
2018-02-27 21:41:03 -0500

    zsh - Restructure detection of uninteresting commands
    Use a `case` statement for the main command. Using a series of `if`
    statements was done while trying to avoid a temporary variable, but
    would get way too complex.

--- zsh.d/R61alert.zsh
+++ zsh.d/R61alert.zsh
@@ -13,18 +13,21 @@ alert_precmd() {
     skip_alert=n
 
     # Don't beep for commands where exiting is uninteresting
-    if [[ $last_cmd[1] == ssh ]] && [[ $#last_cmd = 2 ]]; then
-      # Ignore just ssh to host
-      skip_alert=y
-    elif [[ $last_cmd[1] == sudo ]]; then
-      if [[ $#last_cmd == 1 ]]; then
-        # No arguments
-        skip_alert=y
-      elif [[ $#last_cmd == 3 ]] && [[ $last_cmd[2] == -u ]]; then
-        # Just specifying user
-        skip_alert=y
-      fi
-    fi
+    case "$last_cmd[1]" in
+      (ssh)
+        # Ignore just ssh to host
+        [[ $#last_cmd = 2 ]] && skip_alert=y
+        ;;
+     (sudo)
+       if [[ $#last_cmd == 1 ]]; then
+         # No arguments
+         skip_alert=y
+       elif [[ $#last_cmd == 3 ]] && [[ $last_cmd[2] == -u ]]; then
+         # Just specifying user
+         skip_alert=y
+       fi
+       ;;
+    esac
 
     if [[ $skip_alert == n ]]; then
       echo -n '\a'
