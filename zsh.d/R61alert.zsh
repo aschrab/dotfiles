31a296dc891f492a4aaec9d76b01a90f993c8712
Aaron Schrab <aaron@schrab.com>
2018-02-27 21:00:53 -0500

    zsh - No beep for commands where exit is uninteresting
    When ssh or sudo is used just to get another shell, exiting is almost
    never interesting, so don't issue an alert.

--- zsh.d/R61alert.zsh
+++ zsh.d/R61alert.zsh
@@ -6,7 +6,30 @@ alert_preexec() {
 preexec_functions+='alert_preexec'
 
 alert_precmd() {
-  (( _LAST_ALERT < HISTCMD && SECONDS > 5 )) && echo -n '\a'
+  local last_cmd skip_alert
+
+  if (( _LAST_ALERT < HISTCMD && SECONDS > 5 )); then
+    last_cmd=(${(z)history[$[HISTCMD-1]]})
+    skip_alert=n
+
+    # Don't beep for commands where exiting is uninteresting
+    if [[ $last_cmd[1] == ssh ]] && [[ $#last_cmd = 2 ]]; then
+      # Ignore just ssh to host
+      skip_alert=y
+    elif [[ $last_cmd[1] == sudo ]]; then
+      if [[ $#last_cmd == 1 ]]; then
+        # No arguments
+        skip_alert=y
+      elif [[ $#last_cmd == 3 ]] && [[ $last_cmd[2] == -u ]]; then
+        # Just specifying user
+        skip_alert=y
+      fi
+    fi
+
+    if [[ $skip_alert == n ]]; then
+      echo -n '\a'
+    fi
+  fi
   _LAST_ALERT=$HISTCMD
 }
 precmd_functions+='alert_precmd'
