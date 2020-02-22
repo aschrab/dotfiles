6f5fba9720fe7303fd163665499665e82888bfe8
Aaron Schrab <aaron@schrab.com>
2020-02-21 20:12:53 -0500

    Attempt to load completion for kitty

--- zsh.d/R89compinit.zsh
+++ zsh.d/R89compinit.zsh
@@ -6,6 +6,9 @@ mkdir -p $zsh_cache
 compinit -u -d $zsh_cache/zcomp-$HOST-$ZSH_VERSION
 autoload -U bashcompinit && bashcompinit
 
+# Attempt to load completion for kitty
+(kitty + complete setup zsh 2>/dev/null) | source /dev/stdin
+
 for f in ${ZDOTDIR:-$HOME}/.zshrc $zsh_cache/zcomp-$HOST-$ZSH_VERSION; do
 	zrecompile -p $f && rm -f $f.zwc.old
 done
