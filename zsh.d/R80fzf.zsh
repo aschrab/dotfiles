ba80bfd81a34838173725d922b070c65595e113c
Aaron Schrab <aaron@schrab.com>
2021-04-21 18:08:55 -0400

    zsh - Have fzf use tmux popup when available
    If we detect that tmux is at least version 3.2, and so has support for
    popups ask fzf to use it.

--- zsh.d/R80fzf.zsh
+++ zsh.d/R80fzf.zsh
@@ -5,6 +5,13 @@ fi
 export FZF_TMUX=1
 
 () {
+  (( $+commands[tmux] )) || return
+  local version=$(tmux -V)
+  if is-at-least 3.2 ${version#tmux }
+  then
+    export FZF_TMUX_OPTS=-p
+  fi
+
   local fzf f
   for fzf in /usr/local/opt/fzf/shell /usr/share/doc/fzf/examples
   do
