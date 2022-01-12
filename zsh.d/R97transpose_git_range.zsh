7e20fc097da667dbd6241ea091988bd26d281c69
Aaron Schrab <aaron@schrab.com>
2022-01-12 18:59:08 -0500

    zsh - Add mapping to transpose git ranges

---
+++ zsh.d/R97transpose_git_range.zsh
@@ -0,0 +1,25 @@
+__transpose_git_range() {
+  emulate -L zsh
+  setopt extendedglob
+  autoload match-words-by-style
+
+  local curcontext=":zle:$WIDGET" word done
+  local -a matched_words
+  integer count=${NUMERIC:-1}
+
+  match-words-by-style
+
+  word="$matched_words[2]$matched_words[3]$matched_words[4]$matched_words[5]"
+
+  if [[ $word = *..* ]]; then
+    local begin=${word%%(...|..)*}
+    local ending=${word##*(...|..)}
+    local range=${word%$ending}
+    range=${range#$begin}
+
+    BUFFER="$matched_words[0]$matched_words[1]$ending$range$begin$matched_words[6]"
+  fi
+}
+
+zle -N __transpose_git_range
+bindmodes emacs,viins __transpose_git_range '^X^T'
