4f426990051437ca16627cafbfe752044fb2b55b
Aaron Schrab <aaron@schrab.com>
2023-04-02 18:05:22 -0400

    vim - Rename init.vim to init.lua
    The only reason this had been written with the `.vim` extension is that
    I previously hadn't figured out how to source the main `vimrc.vim` file
    from LUA. Now I've at least found a way to do that.

---
+++ vim/init.lua
@@ -0,0 +1,8 @@
+if os.getenv('XDG_CONFIG_HOME') == '' then
+  vim.cmd [[source $HOME/.config/nvim/vimrc.vim]]
+else
+  vim.cmd [[source $XDG_CONFIG_HOME/nvim/vimrc.vim]]
+end
+
+require('cmp-setup')
+require('lsp-setup')
