cb696ab08359dcef816b2ed0e5471db763bd51c1
Aaron Schrab <aaron@schrab.com>
2023-04-03 06:46:58 -0400

    vim - Fix nvim startup when XDG_CONFIG_HOME isn't set
    I'd handled this being set but empty, but it wasn't handling not being
    set at all.

--- vim/init.lua
+++ vim/init.lua
@@ -1,4 +1,4 @@
-if os.getenv('XDG_CONFIG_HOME') == '' then
+if not os.getenv('XDG_CONFIG_HOME') or os.getenv('XDG_CONFIG_HOME') == '' then
   vim.cmd [[source $HOME/.config/nvim/vimrc.vim]]
 else
   vim.cmd [[source $XDG_CONFIG_HOME/nvim/vimrc.vim]]
