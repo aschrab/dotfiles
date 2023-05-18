2bd5c819cfd96c6b68fe9fc5fe72a1ab5d343ef0
Aaron Schrab <aaron@schrab.com>
2023-05-18 17:56:30 -0400

    vim - Update setup for LSP
    There was a small change in the interface.

--- vim/lua/lsp-setup.lua
+++ vim/lua/lsp-setup.lua
@@ -32,7 +32,7 @@ local lsp_flags = {
   debounce_text_changes = 150,
 }
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 require('lspconfig')['tsserver'].setup{
     on_attach = on_attach,
