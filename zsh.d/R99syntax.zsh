1f3246e9c2f3293db2ca78bb0ffd17cbb9223c40
Aaron Schrab <aaron@schrab.com>
2023-08-07 11:49:55 -0400

    zsh - New filename for syntax highlighting plugin
    When I updated this plugin in 9480c55 (zsh - Update syntax highlighting
    plugin, 2023-07-27) I failed to notice that the actual reason errors
    went away was that I was no longer loading the plugin due to a changed
    file name. Adjust to the new name of the plugin file
    (`F-Sy-H.plugin.zsh`).

--- zsh.d/R99syntax.zsh
+++ zsh.d/R99syntax.zsh
@@ -2,7 +2,7 @@ ZSH_HIGHLIGHT_HIGHLIGHTERS=( main brackets )
 
 () {
   local f
-  for f in ${zshrc_dir:A}/../upstreams/syntax-highlight/{fast,zsh}-syntax-highlighting{,.plugin}.zsh
+  for f in ${zshrc_dir:A}/../upstreams/syntax-highlight/{{fast,zsh}-syntax-highlighting,F-Sy-H}{,.plugin}.zsh
   do
     if [ -r $f ]; then
       source $f
