5809933484d17765dcab123e94d8548a5fc17d8e
Aaron Schrab <aaron@schrab.com>
2024-03-27 11:59:07 -0400

    Mac.sh - Have app switcher shown on all displays
    By default the Cmd+tab app switcher only shows on the display with the
    dock, and that often isn't where I'm looking; enable setting to have it
    shown on all displays for better visibility.

--- Mac.sh
+++ Mac.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Show Cmd+tab app switcher on all displays, not just one with Dock
+defaults write com.apple.dock appswitcher-all-displays -bool true
+killall Dock
+
+# Link VS Code settings into odd place it uses
 realname=$(perl -MCwd -e 'print Cwd::abs_path($ARGV[0])' "$0")
 dotfile_dir=$(dirname "$realname")
 
