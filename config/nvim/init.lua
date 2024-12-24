b820f4d547742763d789f715bd142aa2c84a346f
Aaron Schrab <aaron@schrab.com>
2024-12-24 12:39:17 -0500

    nvim - Basic configuration

---
+++ config/nvim/init.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.scrolloff = 1
+vim.opt.sidescrolloff = 10
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+
+vim.g.mapleader = ','
+
+vim.cmd.colorscheme('vividchalk')
