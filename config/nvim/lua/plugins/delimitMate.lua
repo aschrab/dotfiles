dda98410c1ffaad541143152b33c3eb49a10945f
Aaron Schrab <aaron@schrab.com>
2024-12-24 19:47:05 -0500

    nvim - Copy delimitMate config from vim

--- config/nvim/lua/plugins/delimitMate.lua
+++ config/nvim/lua/plugins/delimitMate.lua
@@ -1,3 +1,12 @@
 return {
-  { "Raimondi/delimitMate" }
+  {
+    "Raimondi/delimitMate",
+    init = function()
+      vim.g.delimitMate_jump_expansion = 1
+      vim.g.delimitMate_expand_space = 1
+      vim.g.delimitMate_expand_cr = 2
+      vim.g.delimitMate_balance_matchpairs = 1
+      vim.g.delimitMate_matchpairs = '(:),[:],{:}'
+    end
+  }
 }
