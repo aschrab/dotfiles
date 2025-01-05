c666df2496e6b8ef87e185d79f236991c9b4aac1
Aaron Schrab <aaron@schrab.com>
2025-01-05 16:01:59 -0500

    nvim - Configure speeddating plugin to work with swapit

--- config/nvim/lua/plugins/speeddating.lua
+++ config/nvim/lua/plugins/speeddating.lua
@@ -1,3 +1,9 @@
 return {
-  { "tpope/vim-speeddating" }
+  {
+    "tpope/vim-speeddating",
+    config = function()
+      vim.keymap.set({ 'n', 'v' }, '<Plug>SwapItFallbackIncrement', '<Plug>SpeedDatingUp')
+      vim.keymap.set({ 'n', 'v' }, '<Plug>SwapItFallbackDecrement', '<Plug>SpeedDatingDown')
+    end
+  }
 }
