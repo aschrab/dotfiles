0520cd0b5eb512ae4d7517dd008a6d3870e79e09
Aaron Schrab <aaron@schrab.com>
2025-01-05 15:08:28 -0500

    nvim - COnfigure unicode plugin to take place of characterize

--- config/nvim/lua/plugins/unicode.lua
+++ config/nvim/lua/plugins/unicode.lua
@@ -1,3 +1,10 @@
 return {
-  { "chrisbra/unicode.vim" }
+  {
+    "chrisbra/unicode.vim",
+    config = function()
+      vim.api.nvim_set_keymap('n', 'ga', ":UnicodeName<CR>", {
+          noremap = true,
+      })
+    end
+  },
 }
