b9489ca9ce635b178a5662cac812914db9b76223
Aaron Schrab <aaron@schrab.com>
2025-01-08 09:21:13 -0500

    nvim - Add fugitive-gitlab plugin

--- config/nvim/lua/plugins/fugitive.lua
+++ config/nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,11 @@
 return {
-  { "tpope/vim-fugitive" }
+  { "tpope/vim-fugitive" },
+  {
+    "shumphrey/fugitive-gitlab.vim",
+    init = function()
+      vim.g.fugitive_gitlab_domains = {
+        ['gitlab.av'] = 'https://gitlab.av'
+      }
+    end
+  }
 }
