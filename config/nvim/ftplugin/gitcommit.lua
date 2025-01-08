4de38af30cb0e441568c269da2d47777d17561f7
Aaron Schrab <aaron@schrab.com>
2025-01-07 20:18:10 -0500

    nvim - Copy over settings for git commit messages
    I especially want to turn on spell checking.

---
+++ config/nvim/ftplugin/gitcommit.lua
@@ -0,0 +1,10 @@
+vim.opt_local.spell = true
+vim.opt_local.spellcapcheck = ""
+
+vim.opt_local.shiftwidth = 2
+vim.opt_local.expandtab = true
+
+vim.opt_local.formatoptions:append('aw')
+vim.opt_local.modeline = false
+vim.opt_local.foldlevel = 99
+vim.opt_local.colorcolumn = '73'
