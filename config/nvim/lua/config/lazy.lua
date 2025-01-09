9dc6fc98bbdda3d7fc6c55f81b137e65c203747a
Aaron Schrab <aaron@schrab.com>
2025-01-08 19:20:13 -0500

    nvim - Turn off configuration change detection
    The reloading doesn't really seem to work, and the notifications are
    **really** annoying.

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -26,4 +26,5 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = false },
+  change_detection = { enabled = false },
 })
