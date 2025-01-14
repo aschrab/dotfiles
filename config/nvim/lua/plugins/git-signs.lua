5e117db36ad4cc767cfcfed2b9582cb6b3f303cb
Aaron Schrab <aaron@schrab.com>
2025-01-14 17:17:09 -0500

    nvim - Add nav mappings for gitsigns plugin

--- config/nvim/lua/plugins/git-signs.lua
+++ config/nvim/lua/plugins/git-signs.lua
@@ -2,7 +2,25 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     init = function()
-      require('gitsigns').setup()
+      signs = require('gitsigns')
+      signs.setup({
+        on_attach = function()
+          vim.keymap.set('n', '[c', function()
+            if vim.wo.diff then
+              vim.cmd.normal({'[c', bang = true})
+            else
+              signs.nav_hunk('prev')
+            end
+          end)
+          vim.keymap.set('n', ']c', function()
+            if vim.wo.diff then
+              vim.cmd.normal({']c', bang = true})
+            else
+              signs.nav_hunk('next')
+            end
+          end)
+        end
+      })
     end
   }
 }
