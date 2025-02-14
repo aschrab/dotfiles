5deb9bf9919caf42dcb56bfd1415c49fb2568e41
Aaron Schrab <aaron@schrab.com>
2025-02-14 12:57:48 -0500

    zsh - Remove setting of $MANPATH
    For some reason this doesn't seem to be working, I get an empty value.
    It also isn't necessary any longer, since modern versions of `man` will
    determine an appropriate value based on `$PATH`. I'd already disabled
    setting it on Mac.

--- zsh.d/E20paths.zsh
+++ zsh.d/E20paths.zsh
@@ -28,20 +28,6 @@ function set_paths {
   )
   export PATH
 
-  if [[ $OSTYPE != darwin* ]]; then
-    typeset -aU manpath
-    manpath=(
-      /usr/local/share/man
-      /usr/local/man
-      /usr/software/man
-      /usr/software/rats/man
-      /usr/software/utils/man
-      /usr/X11/man
-      /usr/share/man
-    )
-    export MANPATH
-  fi
-
   typeset -TUx LD_LIBRARY_PATH ld_library_path
   ld_library_path=()
 }
