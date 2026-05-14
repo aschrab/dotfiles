08e031d2b9f337dfd2d1dc2e6230c52fed88b094
Aaron Schrab <aaron@schrab.com>
2026-05-14 09:35:51 -0400

    Create Mac LaunchAgent to set $PATH
    Arrange for apps launched from GUI to get $PATH that includes Homebrew
    and my own bin directories.
    
    The impetus for this change was that T3 code wasn't finding the GitHub
    CLI installed from Homebrew.

--- setup/Mac.sh
+++ setup/Mac.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+realname=$(perl -MCwd -e 'print Cwd::abs_path($ARGV[0])' "$0")
+dotfile_dir=$(dirname "$(dirname "$realname")")
+
+plist=~/Library/LaunchAgents/user.path.plist
+launchctl unload ~/Library/LaunchAgents/user.path.plist 2>/dev/null || true
+mkdir -p "$(dirname "$plist")"
+perl -pe 's,\$HOME/,'"$HOME/,g" <$dotfile_dir/Library/LaunchAgents/user.path.plist >"$plist"
+launchctl load ~/Library/LaunchAgents/user.path.plist
+
 # Show Cmd+tab app switcher on all displays, not just one with Dock
 if [ "$(defaults read com.apple.dock appswitcher-all-displays)" != 1 ]; then
   defaults write com.apple.dock appswitcher-all-displays -bool true
@@ -7,9 +16,6 @@ if [ "$(defaults read com.apple.dock appswitcher-all-displays)" != 1 ]; then
 fi
 
 # Link VS Code settings into odd place it uses
-realname=$(perl -MCwd -e 'print Cwd::abs_path($ARGV[0])' "$0")
-dotfile_dir=$(dirname "$(dirname "$realname")")
-
 vscode="$HOME/Library/Application Support/Code"
 mkdir -p "$vscode"
 [ -e "$vscode/User/settings.json" ] ||
